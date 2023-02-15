78c27476a19dace7439aa512a85cb57b47a40cd7
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-02-15 00:15:48 -0300

    initial test

--- kit_othello/advsearch/your_agent/agent.py
+++ kit_othello/advsearch/your_agent/agent.py
@@ -1,7 +1,12 @@
 import random
+import time
 from typing import Tuple
 
 from ..othello.gamestate import GameState
+
+PLUS_INF = float('inf')
+MINUS_INF = float('-inf')
+
 
 # Voce pode criar funcoes auxiliares neste arquivo
 # e tambem modulos auxiliares neste pacote.
@@ -19,5 +24,26 @@ def make_move(state: GameState) -> Tuple[int, int]:
     # o codigo abaixo apenas retorna um movimento aleatorio valido para
     # a primeira jogada com as pretas.
     # Remova-o e coloque a sua implementacao da poda alpha-beta
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    #return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    
+    return maxValue(state)
 
+def maxValue(estado):
+    if testeTerminal(estado):
+        return utilidade(estado)
+    v = MINUS_INF
+    for jogada in jogadas:
+        v = max(v, minValue(jogada))
+    return v
+
+
+def minValue(estado):
+    if testeTerminal(estado):
+        return utilidade(estado)
+    v = PLUS_INF
+    for jogada in jogadas:
+        v = min(v, maxValue(jogada))
+    return v
+
+
+    
